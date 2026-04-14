d6c3b65efc40ffbe3763a9edba7667a682556990
Leolagan <122455856@umail.ucc.ie>
2026-04-14 19:34:57 +0100

    Project Clean Up

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>IS4447 Project</Text>
+    </View>
+  );
+}
